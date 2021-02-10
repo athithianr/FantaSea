39b50cebf0a9f499001a0f32829104f2a60c61d3
Athi <arajkumar@securitycompass.com>
2021-02-10 16:44:06 -0500

    add config

---
+++ conf.js
@@ -0,0 +1,6 @@
+module.exports = {
+    'CONSUMER_KEY': "dj0yJmk9REJzb0pXQXpWNk54JmQ9WVdrOWVqQm9kM2RPZG0wbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWY1",
+    'CONSUMER_SECRET': "0f504ab79976fb1695f2e86d57a22b7ed21167c5",
+    'APP_URL': 'https://github.com/athithianr/fantasy-streaming-application',
+    'YAHOO_AUTH_CODE': "zfmrme5"
+  };
