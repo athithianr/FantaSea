70c76246d67ad694810f27968429bb47165315fb
Athi <arajkumar@securitycompass.com>
2021-02-11 20:37:00 -0500

    Add logic to refresh tokens

--- test-yahoo-api.js
+++ test-yahoo-api.js
@@ -1,91 +1,106 @@
+//imported libraries
 const config = require('./conf.js');
 const axios = require("axios");
 const qs = require("qs");
 const fs = require('fs');
 
-
+// secret variables
+const conf = require('./conf.js');
 
 const AUTH_HEADER = Buffer.from(`${config.CONSUMER_KEY}:${config.CONSUMER_SECRET}`).toString(`base64`);
-const myConsole = new console.Console(fs.createWriteStream("/Users/arajkumar/Desktop/fantasy-streaming-application/response.txt"));
 
+const AUTH_FILE = "/Users/arajkumar/Desktop/fantasy-streaming-application/auth.json"
+const AUTH_ENDPOINT= "https://api.login.yahoo.com/oauth2/get_token"
 
-  // const AUTH_HEADER = "ZGoweUptazlSRUp6YjBwWFFYcFdOazU0Sm1ROVdWZHJPV1ZxUW05a00yUlBaRzB3YldOSGJ6bE5RVDA5Sm5NOVkyOXVjM1Z0WlhKelpXTnlaWFFtYzNZOU1DWjRQV1kxOiAwZjUwNGFiNzk5NzZmYjE2OTVmMmU4NmQ1N2EyMmI3ZWQyMTE2N2M1"
-
-// function getAuthorizationURL () {
-//   return axios({
-//     url: `https://api.login.yahoo.com/oauth2/request_auth`,
-//     method: 'get',
-//     headers: {
-//         // 'Authorization': `Basic ${AUTH_HEADER}`,
-//         // 'Content-Type': 'application/x-www-form-urlencoded',
-//         // 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36',
-//     },
-//     data:({
-//         client_id: config.CONSUMER_KEY,
-//         redirect_uri: 'oob',
-//         response_type: 'code'
-//     }),
-//     }).catch((err) => {
-//         console.error(`Error in getAuthorizationURL(): ${err}`);
-//     }).then(resp => console.log(resp))
-    
-// }
-
-
-function getInitialAuthorization () {
-  return axios({
-      url: `https://api.login.yahoo.com/oauth2/get_token`,
-      method: 'post',
-      headers: {
-          'Authorization': `Basic ${AUTH_HEADER}`,
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36',
-      },
-      data: qs.stringify({
-          client_id: config.CONSUMER_KEY,
-          client_secret: config.CONSUMER_SECRET,
-          redirect_uri: 'oob',
-          code: config.YAHOO_AUTH_CODE,
-          grant_type: 'authorization_code'
-      }),
-      }).catch((err) => {
-          console.error(`Error in getInitialAuthorization(): ${err}`);
-      }).then(resp => myConsole.log(resp))
+// Write to an external file to display output data
+function writeToFile(data, file, flag) {
+  if (flag === null) {
+    flag = `a`;
+  }
+  fs.writeFile(file, data, { flag }, (err) => {
+    if (err) {
+      console.error(`Error in writing to ${file}: ${err}`);
+    }
+  });
+  return 1;
 }
 
 
-// Hit the Yahoo Fantasy API
-async function makeAPIrequest (url) {
+
+function getInitialAuthorization() {
+  return axios({
+    url: `https://api.login.yahoo.com/oauth2/get_token`,
+    method: 'post',
+    headers: {
+      'Authorization': `Basic ${AUTH_HEADER}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36',
+    },
+    data: qs.stringify({
+      client_id: config.CONSUMER_KEY,
+      client_secret: config.CONSUMER_SECRET,
+      redirect_uri: 'oob',
+      code: config.YAHOO_AUTH_CODE,
+      grant_type: 'authorization_code'
+    }),
+  }).catch((err) => {
+    console.error(`Error in getInitialAuthorization(): ${err}`);
+  }).then(resp => myConsole.log(resp))
+}
+
+function refreshAuthorizationToken(token) {
+  return axios({
+    url: AUTH_ENDPOINT,
+    method: "post",
+    headers: {
+      Authorization: `Basic ${AUTH_HEADER}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36",
+    },
+    data: qs.stringify({
+      redirect_uri: "oob",
+      grant_type: "refresh_token",
+      refresh_token: token,
+    }),
+  }).catch((err) => {
+    console.error(`Error in refreshAuthorizationToken(): ${err}`);
+  });
+}
+
+
+async function makeAPIrequest(url) {
   let response;
   try {
-      response = await axios({
-      url: url,
-          method: 'get',
-          headers: {
-              'Authorization': `Bearer ${config.ACESS_TOKEN}`,
-              'Content-Type': 'application/x-www-form-urlencoded',
-              'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36',
-          },
-          timeout: 10000,
-      });
-      const jsonData = JSON.parse(parser.toJson(response.data));
-      return jsonData;
+    response = await axios({
+      url,
+      method: "get",
+      headers: {
+        Authorization: `Bearer ${conf.ACESS_TOKEN}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36",
+      },
+    });
+    const jsonData = JSON.parse(parser.toJson(response.data));
+    return jsonData
   } catch (err) {
-      if (err.response.data && err.response.data.error && err.response.data.error.description && err.response.data.error.description.includes("token_expired")) {
-          const newToken = await refreshAuthorizationToken(CREDENTIALS.refresh_token);
-          if (newToken && newToken.data && newToken.data.access_token) {
-              CREDENTIALS = newToken.data;
-              // Just a wrapper for fs.writeFile
-              writeToFile(JSON.stringify(newToken.data), AUTH_FILE, 'w');
-              return makeAPIrequest(url, newToken.data.access_token, newToken.data.refresh_token);
-
-           }
-      } else {
-          console.error(`Error with credentials in makeAPIrequest()/refreshAuthorizationToken(): ${err}`);
-          process.exit();
+    console.log(err)
+    if (err.response.data && err.response.data.error && err.response.data.error.description && err.response.data.error.description.includes("token_expired")) {
+      const newToken = await refreshAuthorizationToken(config.REFRESH_TOKEN);
+      console.log(newToken)
+      if (newToken && newToken.data && newToken.data.access_token) {
+        // this.CREDENTIALS = newToken.data;
+        writeToFile(JSON.stringify(newToken.data), AUTH_FILE, "w");
+        return makeAPIrequest(url, newToken.data.access_token, newToken.data.refresh_token);
       }
+    } else {
+      console.error(`Error with credentials in makeAPIrequest()/refreshAuthorizationToken(): ${err}`);
+    }
+    return err;
   }
 }
 
-const resp = getInitialAuthorization();
+
+// const resp = getInitialAuthorization();
+const res = makeAPIrequest('https://fantasysports.yahooapis.com/fantasy/v2/team/223.l.431.t.1');
+
 
